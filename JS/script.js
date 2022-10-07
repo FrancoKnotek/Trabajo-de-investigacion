d35483cf471564949525e73b6d9d8856c49583dc
unknown <francoknotek27@gmail.com>
2022-10-07 19:06:48 -0300

    creando variable y condicional

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,10 @@
 var datos = function() {
     var numero = document.getElementById("numero").value;
-    console.log(numero)
+    console.log(numero);
+    var verificacion = document.getElementById("verificacion").value;
+    if (verificacion = 15) {
+        console.log("No es un robot")
+    } else {
+        console.log("Es un robot")
+    }
 }
